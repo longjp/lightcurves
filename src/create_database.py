d1da33969ba936204efa1badfcc00e1258c2fa44
james <james@james-laptop.(none)>
2011-03-24 15:02:58 -0700

    updated enter_record function so can put in whatever columns i want in sources table, added assembleSQLCommand function to do this. this will break some code but should be very easy to fix

--- src/create_database.py
+++ src/create_database.py
@@ -19,8 +19,8 @@ from time import time
 ### notes / improvements
 ###
 ##
-## 1. redo testing in the if statements below
-## 2. get rid of the measurements table, keep these in xml files?
+## 1. enter record like functions should accept name of columns so that 
+##   function with different amounts of data can call them
 ##
 
 
@@ -42,9 +42,18 @@ def insert_measurements(cursor,last_id,measurements):
         row_list.append(last_id)
         cursor.execute(sql_cmd,row_list)
 
+# puts together an sql insert query - specify table and columns to enter
+def assembleSQLCommands(table_name,curve_info_names):
+    sql_cmd = """insert into """ + table_name +  """(""" + ','.join(curve_info_names) + """) values (""" + ('?,' * len(curve_info_names))[:-1] + """)"""
+    return(sql_cmd)
+
 # loads data into sources and measurements, used by ingest_xml
-def enter_record(curve_info,tfe,cursor,original_number=False):
-    sql_cmd = """insert into sources(number_points, classification, c1, e1, c2, e2, raw_xml,survey,xml_filename,date) values (?,?,?,?,?,?,?,?,?,?)"""
+def enter_record(curve_info,curve_info_names,tfe,cursor,original_number=False):
+    # earlier we used line below, all references to this function that
+    # haven't been changed should have these names put in argument
+    # curve_info_names
+    #sql_cmd = """insert into sources(number_points, classification, c1, e1, c2, e2, raw_xml,survey,xml_filename,date) values (?,?,?,?,?,?,?,?,?,?)"""
+    sql_cmd = assembleSQLCommand("sources",curve_info_names)
     cursor.execute(sql_cmd, curve_info)
 
     # find last insert number
@@ -161,12 +170,13 @@ def ingest_many_xml(folder,cursor,connection,survey='', \
 # creates table sources and table measurements if they do not exist
 # deletes all records if REMOVE_RECORDS=TRUE
 def create_db(cursor,features_file=False,REMOVE_RECORDS=False):
-        sql_cmd = """CREATE TABLE IF NOT EXISTS sources (source_id INTEGER PRIMARY KEY AUTOINCREMENT, original_source_id INTEGER, noisification REAL DEFAULT NULL,number_points INTEGER, date TEXT,classification TEXT,survey TEXT,c1 REAL,e1 REAL,c2 REAL,e2 REAL,xml_filename TEXT,raw_xml TEXT);"""
+        sql_cmd = """CREATE TABLE IF NOT EXISTS sources (source_id INTEGER PRIMARY KEY AUTOINCREMENT, original_source_id INTEGER, noisification REAL DEFAULT NULL,number_points INTEGER, date TEXT,classification TEXT,survey TEXT,true_period REAL DEFAULT NULL,c1 REAL,e1 REAL,c2 REAL,e2 REAL,xml_filename TEXT,raw_xml TEXT);"""
         cursor.execute(sql_cmd)        
         sql_cmd = """CREATE TABLE IF NOT EXISTS measurements (measurements_id INTEGER PRIMARY KEY AUTOINCREMENT, time REAL, flux REAL, error REAL, source_id INTEGER, FOREIGN KEY(source_id) REFERENCES sources(source_id));"""
         cursor.execute(sql_cmd)
         if features_file == False:
             print "Could not create / check existence of features table because no filename for features names was provided"
+
         else:
             try:
                 f = open(features_file,'r')
