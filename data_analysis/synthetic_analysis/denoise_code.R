4bee175ea1492c7f6983ad4d602882d3020617ac
james <james@james-laptop.(none)>
2011-05-14 19:59:27 -0700

    have moved most denoisification functions out of this file into denoisification.R and rf_denoise.R. this now has a lot of graphics functions, ect.

--- data_analysis/synthetic_analysis/denoise_code.R
+++ data_analysis/synthetic_analysis/denoise_code.R
@@ -1,25 +1,30 @@
 ####
 #### by James Long
-#### date May 3, 2011
+#### date May 12, 2011
 ####
 #### used for studying denoisification on synthetic data
 ####
 
 
-# program setup
+## program setup
 rm(list=ls(all=TRUE))
 set.seed(22071985)
 
+## may want to load saved data
+##load('denoise_code.RData')
+
+source('denoisification.R')
 source('../noisification_code/Rfunctions.R')
+source('rf_denoise.R')
 library('randomForest')
 library('rpart')
 library('xtable')
 
-# set the output graphics folder
+## set the output graphics folder
 graphics = fileOutLoc('figures/')
 tables = fileOutLoc('tables/')
 
-# get the data
+## get the data
 features = '../../data_processed/synthetic_analysis/sources00001.dat'
 tfe = '../../data_processed/synthetic_analysis/tfe00001.dat'
 data1 = read.table(features,sep=';',header=TRUE)
@@ -74,17 +79,32 @@ points.levels
 class.names = names(table(data1$sources.classification))
 
 
-
-
-
-
 ####
 #### denoisification by RF regression
 ####
 
-imp.variables = c("features.p2p_scatter_over_mad","features.small_kurtosis","features.p2p_scatter_2praw","features.qso_log_chi2nuNULL_chi2nu","features.percent_difference_flux_percentile","features.qso_log_chi2_qsonu","features.flux_percentile_ratio_mid20","features.flux_percentile_ratio_mid35","features.flux_percentile_ratio_mid80","features.flux_percentile_ratio_mid50","features.skew","features.fold2P_slope_10percentile","features.amplitude","features.p2p_scatter_pfold_over_mad","features.stetson_j","features.stetson_k","features.medperc90_2p_p","features.freq1_harmonics_freq_0","features.std","features.freq1_harmonics_rel_phase_1","features.scatter_res_raw","features.freq_signif")
-
-
+imp.variables = c("features.p2p_scatter_over_mad",
+  "features.small_kurtosis",
+  "features.p2p_scatter_2praw",
+  "features.qso_log_chi2nuNULL_chi2nu",
+  "features.percent_difference_flux_percentile",
+  "features.qso_log_chi2_qsonu",
+  "features.flux_percentile_ratio_mid20",
+  "features.flux_percentile_ratio_mid35",
+  "features.flux_percentile_ratio_mid80",
+  "features.flux_percentile_ratio_mid50",
+  "features.skew",
+  "features.fold2P_slope_10percentile",
+  "features.amplitude",
+  "features.p2p_scatter_pfold_over_mad",
+  "features.stetson_j",
+  "features.stetson_k",
+  "features.medperc90_2p_p",
+  "features.freq1_harmonics_freq_0",
+  "features.std",
+  "features.freq1_harmonics_rel_phase_1",
+  "features.scatter_res_raw",
+  "features.freq_signif")
 
 GetFormula = function(){
   data1_features = names(data1)[grep("features.*",names(data1))]
@@ -98,177 +118,6 @@ GetFormula = function(){
     paste(data1_features,collapse=" + ")))
   return(rf_formula)
 }
-  
-
-
-##
-## predict noisy version of clean curves, specifically
-## predict each of the features in y.vars, using the
-## features x.vars
-##
-PredictResponses = function(x.temp,y.temp,x.vars,y.vars){
-  rfs = list()
-  for(i in 1:length(y.vars)){
-    print(sprintf("run %s / %s",i,length(y.vars)))
-    rfs[[i]] = randomForest(x.temp[,x.vars],
-         y.temp[,y.vars[i]],
-         nodesize=1)
-  }
-  y.hat = matrix(0,nrow=nrow(x.temp),ncol=length(y.vars))
-  y.sds = matrix(0,nrow=nrow(x.temp),ncol=length(y.vars))
-  for(i in 1:length(y.vars)){
-    y.hat[,i] = predict(rfs[[i]])
-    preds = predict(rfs[[i]],newdata=x.temp,predict.all=TRUE)[[2]]
-    y.sds[,i] = apply(preds,1,sd) / .33
-  }
-  return(list(rfs,y.hat,y.sds))
-}
-
-
-
-## y = (# test) x (# features) from test
-## y.hat = (# train) x (#features) from train
-## output (# train) x (# test) x (# features)
-##    where differences are taken between every
-##    pair of training and test obs
-YminusYhat = function(y,y.hat){
-  result = array(0,dim=c(nrow(y.hat),nrow(y),ncol(y.hat)))
-  for(i in 1:(dim(result)[2])){
-    v.y = y[i,]
-    result[,i,] = t(apply(y.hat,1,function(x){abs(x - v.y)}))
-  }
-  return(result)
-}
-
-## TEST: YminusYhat - seems to work
-a = matrix(c(1,2,3,4),ncol=2)
-a
-b = matrix(c(10,1,15,8,0,12),ncol=2)
-b
-c = YminusYhat(a,b)
-dim(c)
-c[,1,]
-c[,2,]
-
-
-## divide all the differences in big.raw by
-## their standard deviations
-StandardizeBigRaw = function(big.raw,sds){
-  big.norm = array(0,dim=dim(big.raw))
-  if(!is.matrix(sds)){
-    print('sds must be a matrix')
-    return(0)
-  }
-  ## if a single standard deviation for all obs, just repeat it
-  if(dim(sds)[1] == 1){
-    sds = matrix(rep(sds,dim(big.raw)[1]),
-      nrow=dim(big.raw)[1],byrow=TRUE)
-  }
-  ## make sure sds conforms to dimensions of big.raw
-  if(!identical(dim(sds),dim(big.raw)[c(1,3)])){
-    print("sds wrong dimensions")
-    stop
-  }
-  for(i in 1:(dim(big.raw)[2])){
-    big.norm[,i,] = big.raw[,i,] / sds
-  }
-  return(big.norm)
-}
-
-## TEST: StandardizeBigRaw - seems to work
-c[,1,]
-c[,2,]
-sds = array(c(.5,1),dim=c(1,2))
-sds
-dim(sds)
-c.norm = StandardizeBigRaw(c,sds)
-dim(c)
-dim(c.norm)
-c.norm[,1,]
-c[,1,]
-c.norm[,2,]
-c[,2,]
-
-##
-## tie all the function together
-##
-
-## get the correctly noisified training data
-Denoise = function(data1train.clean,
-                   data1train.temp,
-                   data1test.temp,
-                   imp.variables,
-                   return.all=FALSE){
-
-  ## are we good?
-  if(!identical(data1train.temp$sources.original_source_id,
-            data1train.clean$sources.original_source_id)){
-    print("clean training and temp training don't match")
-    return(0)
-  }
-
-  ## convert data frames to matrices so things work fast
-  data1train.clean.matrix = as.matrix(data1train.clean[,imp.variables])
-  data1train.temp.matrix = as.matrix(data1train.temp[,imp.variables])
-  data1test.temp.matrix = as.matrix(data1test.temp[,imp.variables])
-
-
-  ## BEGIN: generate pyx
-  
-  ## do all the randomForest regressions
-  stuff = PredictResponses(data1train.clean.matrix,
-    data1train.temp.matrix,imp.variables,imp.variables)
-
-  ## get all the residuals
-  big.raw = YminusYhat(data1test.temp.matrix,stuff[[2]])
-
-  ## get standard errors and normalize big.raw
-  sds = matrix(colMeans(stuff[[3]]),nrow=1)
-  big.norm = StandardizeBigRaw(big.raw,sds)
-
-  ## take the product across all the features i.e.
-  ## p(y_1|x) x . . . x p(y_m|x) = p(y|x)
-  ## should be (#train) x (# test)
-  ## 1 column for each test
-  pyx = apply(big.norm,c(1,2),function(x){prod(dnorm(x))})
-
-  ## END: Generate pyx
-  
-  ## construct rf on clean data and get probabilities
-  rf_formula = GetFormula()
-  rf.clean = randomForest(rf_formula,data=data1train.clean)
-  pzx = predict(rf.clean,type='vote')
-  class.names = colnames(pzx)
-
-  ## change pyx and pzx into predictions
-  predictions = class.names[apply(pyx,2,function(x)
-    {which.max(apply(pzx,2,function(y){sum(x*y)}))})]
-  result = mean(predictions !=
-           data1test.temp$sources.classification)
-  if(!return.all){
-    return(result)
-  }
-  result = list("error"=result,"rfs"=stuff[[1]],
-    "pzx"=pzx,"pyx"=pyx,"y.hat"=stuff[[2]],
-    "y.sds"=stuff[[3]])
-}
-
-##
-## select the correct training and test
-## data for denoisification
-PrepareDataForDenoise = function(i){
-  ## get the correct training data
-  data1train.temp = subset(data1train,
-    features.n_points == points.levels[i] &
-    row_id==0 & !contains.random)
-  data1train.temp = data1train.temp[
-    order(data1train.temp$sources.original_source_id),]
-
-  ## get the correct test set
-  data1test.temp = subset(data1,
-    sources.survey=='test' & features.n_points == points.levels[i])
-  return(list(data1train.temp,data1test.temp))
-}
 
 
 
@@ -276,7 +125,7 @@ PrepareDataForDenoise = function(i){
 
 
 ###
-### run Denoise function
+### run Denoisification function
 ###
 
 ## get the clean training set
@@ -301,226 +150,110 @@ for(i in 1:length(points.levels)){
   denoised.data = PrepareDataForDenoise(i)
 
   ## get all the results
-  total.results[[i]] = Denoise(data1train.clean,denoised.data[[1]],
-                 denoised.data[[2]],imp.variables)
+  total.results[[i]] = Denoisification(data1train.clean,
+                 denoised.data[[1]],
+                 denoised.data[[2]],
+                 imp.variables,method=DenoiseRF)
 }
 
 
+## save current results
+save.image(file='denoise_code.RData')
 
 ## get results from noisification
 load('RData/randomForestNoisificationResults.RData')
 source('../noisification_code/Rfunctions.R')
 
 total.results = matrix(total.results,nrow=1)
+total.results
 total.results.sd = computeStandardErrors(total.results,500,sderror=1)
 errorsSD[2,,] = total.results.sd[1,,]
-
-
+data.to.plot = errorsSD[c(1,2,4),,]
+  
 pdf(graphics('denoiseNoiseComp.pdf'))
-plotLines(errorsSD[c(1,2,4),,],points.levels,xlab="Number of Flux Measurements",ylab="Error",ymin=0,maintitle="Random Forests")
+plotLines(data.to.plot,points.levels,xlab="Number of Flux Measurements",ylab="Error",ymin=0,maintitle="Random Forests")
 legend(60, .5,c("Naive","RF Denoisification","5 x Noisification RF"),col=1:length(class.names),lwd=2,cex=1,title="Classifiers",pch=1:length(class.names))
 dev.off()
 
 
 
 
-## get full information about 20 flux case
-denoised.data = PrepareDataForDenoise(2)
-everything.20 = Denoise(data1train.clean,denoised.data[[1]],
-                 denoised.data[[2]],imp.variables,
+##
+## Study 20 flux noise case in detail
+##
+selected.data = PrepareDataForDenoise(2)
+everything.20 = Denoisification(data1train.clean,selected.data[[1]],
+                 selected.data[[2]],imp.variables,
                  return.all=TRUE)
 
 save.image(file='denoise_code.RData')
 
-###
-### ran up to here
-### 
-
-stop
-
-###
-### what sort of things should I analyze in this algorithm ?
-### 
-### 1. what features are most important for predicting
-###    which features?
-###     - what about trying just real feature to predict noisy
-###     - might be possible to get really good estimates of
-###        standard error that way
-### 2. how realistic are my estimates of SE?
-### 3. stability to using different sets of features for p(y|x)?
-### 4. what are sizes of p(y|x_i)'s, is a single x_i dominating?
-### 5. do individual errors follow a normal dist?
-###     i.e. predicting y_feat1 using x_1, . . . ,x_n,
-###    
+class(everything.20)
+summary(everything.20)
 
 
+## a few of these are interesting
+## it appears that 50 median diff
+## is very good at predicting
+## other median diff
+par(ask=TRUE)
+for(i in 1:length(everything.20[[2]])){
+  varImpPlot(everything.20[[2]][[i]],main=imp.variables[i])
+}
+summary(everything.20)
+class(everything.20$pyx)
+dim(everything.20$pyx)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-Imp.variables = c("features.p2p_scatter_over_mad","features.small_kurtosis","features.p2p_scatter_2praw","features.qso_log_chi2nuNULL_chi2nu","features.percent_difference_flux_percentile","features.qso_log_chi2_qsonu","features.flux_percentile_ratio_mid20","features.flux_percentile_ratio_mid35","features.flux_percentile_ratio_mid80","features.flux_percentile_ratio_mid50","features.skew","features.fold2P_slope_10percentile","features.amplitude","features.p2p_scatter_pfold_over_mad","features.stetson_j","features.stetson_k","features.medperc90_2p_p","features.freq1_harmonics_freq_0","features.std","features.freq1_harmonics_rel_phase_1","features.scatter_res_raw","features.freq_signif")
-imp.variables = imp.variables[1:5]
-for(i in 1:length(imp.variables)){
-  print(imp.variables[i])
-  print(length(unique(data1train.temp[,imp.variables[i]])))
+Ratio = function(x,quant=.98){
+  f1 = ecdf(x)
+  top = x[f1(x) > quant]
+  ratio = min(top) / max(top)
+  return(ratio)
 }
 
 
 
+ds = apply(everything.20$pyx,2,function(x){Ratio(x,.997)})
+ds.density = density(ds)
+pdf(graphics('pxyRatiosDensityTop2.pdf'))
+plot(ds.density,main='',xlab='Ratio largest (across i) p(y|x_i) to 2nd largest (across i) p(y|x_i)')
+dev.off()
+
+ds = apply(everything.20$pyx,2,function(x){Ratio(x,.99)})
+ds.density = density(ds)
+pdf(graphics('pxyRatiosDensityTop5.pdf'))
+plot(ds.density,main='',xlab='Ratio largest (across i) p(y|x_i) to 5th largest (across i) p(y|x_i)')
+dev.off()
+
+ds = apply(everything.20$pyx,2,function(x){Ratio(x,.90)})
+ds.density = density(ds)
+pdf(graphics('pxyRatiosDensityTop50.pdf'))
+plot(ds.density,main='',xlab='Ratio largest (across i) p(y|x_i) to 50th largest (across i) p(y|x_i)')
+dev.off()
+
+
+
+summary(everything.20)
+class(everything.20$y.sds)
+dim(everything.20$y.sds)
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-WrapLines = function(){
-   nNN = 5
-   neighbors = array(0,dim=c(length(unique(data1test$sources.original_source_id)),nNN,length(points.levels)))
-   imp.variables = c("features.freq1_harmonics_freq_0","features.amplitude")
-   ##imp.variables = c("features.beyond1std","features.max_slope","features.skew","features.amplitude","features.freq1_harmonics_freq_0")
-   ##imp.variables = c("features.p2p_scatter_over_mad","features.small_kurtosis","features.p2p_scatter_2praw","features.beyond1std","features.max_slope","features.qso_log_chi2nuNULL_chi2nu","features.percent_difference_flux_percentile","features.qso_log_chi2_qsonu","features.flux_percentile_ratio_mid20","features.flux_percentile_ratio_mid35","features.flux_percentile_ratio_mid80","features.flux_percentile_ratio_mid50","features.skew","features.fold2P_slope_10percentile","features.amplitude","features.p2p_scatter_pfold_over_mad","features.stetson_j","features.stetson_k","features.medperc90_2p_p","features.freq1_harmonics_freq_0","features.median_buffer_range_percentage","features.std","features.freq1_harmonics_rel_phase_1","features.scatter_res_raw","features.freq_signif")
-
-   ## now do euclidean distance
-   for(i in 1:(dim(neighbors)[3])){
-
-     ## get the test data looking pretty
-     data1test.temp = subset(data1test,
-       features.n_points==points.levels[i])
-     data1test.temp = data1test.temp[
-       order(data1test.temp$sources.original_source_id),]
-     data1test.temp = subset(data1test.temp,select=imp.variables)
-     data1test.temp = as.matrix(data1test.temp)
-
-     ## get the training data looking pretty
-     data1train.temp = subset(data1train,
-       features.n_points == points.levels[i] &
-       !contains.random)
-     data1train.temp = data1train.temp[
-       order(data1train.temp$sources.original_source_id),]
-     ordered.train.ids = data1train.temp$sources.original_source_id
-     data1train.temp = subset(data1train.temp,select=imp.variables)
-     data1train.temp = as.matrix(data1train.temp)
-
-     ## standardize everything
-     ecdf.list = apply(data1train.temp,2,ecdf)
-     for(j in 1:length(ecdf.list)){
-       data1train.temp[,j] = ecdf.list[[j]](data1train.temp[,j])
-       data1test.temp[,j] = ecdf.list[[j]](data1test.temp[,j])
-     }
-
-     ## compute the distances for each test observation
-     for(j in 1:(dim(neighbors)[1])){
-       print(j)
-       current.vec = data1test.temp[j,]
-       distances = apply(data1train.temp,
-         1,function(z) { sum((z - current.vec)^2)})
-       neighbors[j,,i] = ordered.train.ids[
-                  order(distances) < (nNN + .5)]
-     }
-
-   ## report on progress
-   print("done with round:")
-   print(i)
-
- }
- return(neighbors)
+for(i in 1:length(everything.20[[2]])){
+  d1 = density(everything.20$y.sds[,i])
+  pdf(graphics(paste("residuals",i,".pdf",sep="")))
+  plot(d1,lwd=2,main=paste(imp.variables[i],"Residuals"),sub="Dotted Line is Normal Fit")
+  curve(dnorm(x,mean=0,sd=sd(everything.20$y.sds[,i])),
+        col="red",add=T,lwd=2,lty=2)
+  dev.off()
 }
 
-neighbors = WrapLines()
+
+summary(everything.20)
+class(everything.20$pyx)
+dim(everything.20$pyx)
 
 
-## use the correct features -> TODO: move so that both
-## noisification and denoisification use same code
-data1_features = names(data1)[grep("features.*",names(data1))]
-to_remove = c("features.n_points","features.source_id",
-  "features.max_slope","features.min",
-  "features.linear_trend","features.max",
-  "features.weighted_average","features.median")
-data1_features = data1_features[!(data1_features %in%
-  to_remove)]
-rf_formula = formula(paste("sources.classification ~ ",
-  paste(data1_features,collapse=" + ")))
-rf_formula
-
-## construct a forest on the clean data
-data1train.temp = subset(data1train,
-  sources.original_source_id == features.source_id)
-rf = randomForest(rf_formula,data=data1train.temp)
-
-rf.predictions = predict(rf,newdata=data1train.temp,type='prob')
-rownames(rf.predictions) = data1train.temp$features.source_id
-
-classification = function(ids){
-  preds = rf.predictions[as.character(ids),]
-  most.likely = colnames(preds)[which.max(apply(preds,2,mean))]
-  return(most.likely)
-}
-
-## get the class predictions based on the nearest neighbors
-ans = apply(neighbors,c(1,3),classification)
-dim(ans)
-head(ans)
-
-## get the truth
-data1test.temp = subset(data1test,
-  subset=features.n_points == points.levels[1])
-truth = data1test.temp[order(
-  data1test.temp$sources.original_source_id),
-  c("sources.classification")]
-
-## compute and display the error
-errors = apply(ans,2,function(x) { mean(x != truth)})
-print("the errors are:")
-print(errors)
-
-## essentially becomes a NN classifier
-
-##
-## we should compare to naive, and 1-noisified RF
-##
-
+save.image(file='denoise_code.RData')
+load(file='denoise_code.RData')
