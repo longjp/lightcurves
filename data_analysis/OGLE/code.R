1ee55a1487421faf495b62a3771aa4de650d9b3e
james <james@james-laptop.(none)>
2011-07-08 20:33:53 -0700

    started work on creating 3d set of histograms. this code will probably be moved to Rfunctions.R at some point

--- data_analysis/OGLE/code.R
+++ data_analysis/OGLE/code.R
@@ -7,6 +7,7 @@
 ########## 
 
 
+
 # program setup
 rm(list=ls(all=TRUE))
 set.seed(22071985)
@@ -16,6 +17,7 @@ source('../denoisification_code/denoisification.R')
 source('../denoisification_code/rf_denoise.R')
 library('randomForest')
 library('rpart')
+library('MASS')
 library('xtable')
 
 # set the output graphics folder
@@ -24,19 +26,47 @@ tables = fileOutLoc('tables/')
 RData = fileOutLoc('RData/')
 
 
-# get the data
+## get the data
 features = '../../data_processed/OGLE/sources00001.dat'
 tfe = '../../data_processed/OGLE/tfe00001.dat'
 data1 = read.table(features,sep=';',header=TRUE)
 time_flux = read.table(tfe,sep=';',header=TRUE)
 
 sum(data1$sources.survey == 'test') / 11
-sum(data1$sources.survey == 'train' & data1$features.source_id == data1$sources.original_source_id)
+sum(data1$sources.survey == 'train' & data1$features.source_id
+    == data1$sources.original_source_id)
+
+### BEGIN NOT IMPORTANT
+## simple comparison of LDA and random forest on clean data
+## this code is not too important
+data1clean = subset(data1,
+  features.source_id==sources.original_source_id)
+data1cleantrain = subset(data1clean,sources.survey=="train")
+data1cleantest = subset(data1clean,sources.survey=="test")
+nrow(data1clean)
+data1_features = names(data1)[grep("features.*",names(data1))]
+to_remove = c("features.n_points","features.source_id",
+  "features.max_slope","features.min",
+  "features.linear_trend","features.max",
+  "features.weighted_average","features.median",
+  "features.freq1_harmonics_rel_phase_0")
+data1_features = data1_features[!(data1_features %in%
+  to_remove)]
+rf_formula = formula(paste("sources.classification ~ ",
+  paste(data1_features,collapse=" + ")))
+rf_formula
+rf = randomForest(rf_formula,data1cleantrain)
+lda.fit = lda(rf_formula,data1cleantrain)
+lda.predict = predict(lda.fit,newdata=data1cleantest)
+rf.predict = predict(rf,newdata=data1cleantest)
+mean(lda.predict$class != data1cleantest$sources.classification)
+mean(rf.predict != data1cleantest$sources.classification)
+### END NOT IMPORTANT
 
 
 
-# necessary for real data b/c there is no definition of true period
-# we DEFINE true_period to be period of clean sources
+## necessary for real data b/c there is no definition of true period
+## we DEFINE true_period to be period of clean sources
 true_period_df = subset(data1,
   subset=features.source_id==sources.original_source_id,
   select=c("sources.original_source_id",
@@ -48,11 +78,9 @@ data1$sources.true_period = NULL
 data2 = merge(data1,true_period_df)
 data1 = data2
 
+
 ## run noisification code
 source('../noisification_code/noisification_analysis.R')
-
-
-
 
 ## run robust code
 source('../robust_code/robust.R')
@@ -62,6 +90,8 @@ source('../denoisification_code/denoise_code.R')
 
 
 
+#### create set of kdes that joey had in his paper
+#### TODO: finalize this code and move to Rfunctions.R file
 
 
 
