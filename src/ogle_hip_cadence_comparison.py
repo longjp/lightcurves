1b12c20521409b1ee79621bbe6fb3927ae7a0094
james <james@james-laptop.(none)>
2011-08-08 18:47:49 -0700

    not sure how this changed

--- src/ogle_hip_cadence_comparison.py
+++ src/ogle_hip_cadence_comparison.py
@@ -38,243 +38,10 @@ def tolist(db_info):
  return(list1)
 
 
-## MAKE HIPPARCOS CADENCES DATABASE
-## make and test connection to the database
-db_filename = '../db/hipparcos_cadences.db'
-features_file = "../db/derived_features_list.txt"
-connection = sqlite3.connect(db_filename)
-cursor = connection.cursor()
-create_database.create_db(cursor,features_file=features_file,
-                          REMOVE_RECORDS=True)
-connection.commit()
-folder = "../data/debosscher"
-create_database.ingest_many_xml(folder,cursor,connection,
-                                survey="hipparcos",
-                                number_processors=2)
-connection.commit()
 
 
-
-
-
-
-## examine what we have collected
-sql_cmd = """SELECT source_id,survey,number_points,classification FROM sources"""
-cursor.execute(sql_cmd)
-db_info = cursor.fetchall()
-for i in db_info:
-    print i
-
-print len(db_info)
-
-##
-## remove everything not in an hipparcos, first select OGLE sources
-##
-OGLE = ("Multiple Mode Cepheid","RR Lyrae, Double Mode","Algol (Beta Persei)","Beta Lyrae","W Ursae Majoris")
-
-## make sure you are selecting the correct sources before deleting
-sql_cmd = """SELECT source_id FROM sources WHERE Classification IN""" + repr(OGLE)
-cursor.execute(sql_cmd)
-db_info = cursor.fetchall()
-for i in db_info:
-    print i
-
-## should be 517 if joey's email is correct AND my code is correct
-print(len(db_info))
-
-ogle_sources = tolist(db_info)
-
-
-
-## delete OGLE sources from sources and measurements tables
-sql_cmd = """DELETE FROM sources WHERE classification IN""" + repr(OGLE)
-cursor.execute(sql_cmd)
-sql_cmd = """DELETE FROM measurements WHERE source_id IN """ + repr(tuple(ogle_sources))
-cursor.execute(sql_cmd)
-connection.commit()
-
-
-sql_cmd = """SELECT count(*) FROM measurements"""
-cursor.execute(sql_cmd)
-db_info = cursor.fetchall()
-db_info
-sql_cmd = """SELECT count(DISTINCT source_id) FROM measurements"""
-cursor.execute(sql_cmd)
-db_info = cursor.fetchall()
-db_info
-sql_cmd = """SELECT count(*) FROM sources"""
-cursor.execute(sql_cmd)
-db_info = cursor.fetchall()
-db_info
-
-
-## remove everything with fewer than 100 flux measurements
-sql_cmd = """SELECT number_points FROM sources"""
-cursor.execute(sql_cmd)
-db_info = cursor.fetchall()
-for i in db_info:
-    print i
-
-len(db_info)
-db_info = tolist(db_info)
-
-sumList = 0
-for i in db_info:
-	sumList = i + sumList
-
-float(sumList) / len(db_info)
-
-
-sql_cmd = """SELECT source_id FROM sources WHERE number_points < 100"""
-cursor.execute(sql_cmd)
-db_info = cursor.fetchall()
-db_info
-
-db_info = tolist(db_info)
-sql_cmd = """DELETE FROM measurements WHERE source_id IN""" + repr(tuple(db_info))
-cursor.execute(sql_cmd)
-
-sql_cmd = """DELETE FROM sources WHERE number_points < 100"""
-cursor.execute(sql_cmd)
-
-## ::check what we did::
-## see how many sources in measurements and how many sources in sources
-sql_cmd = """SELECT count(DISTINCT source_id) FROM measurements"""
-cursor.execute(sql_cmd)
-db_info = cursor.fetchall()
-db_info
-sql_cmd = """SELECT count(*) FROM sources"""
-cursor.execute(sql_cmd)
-db_info = cursor.fetchall()
-db_info
-
-
-
-##############
-connection.commit()
-connection.close()
-
-
-
-
-
-
-## OGLE CADENCES DATABASE
-## make and test connection to the database
-db_filename = '../db/ogle_cadences.db'
-features_file = "../db/derived_features_list.txt"
-connection = sqlite3.connect(db_filename)
-cursor = connection.cursor()
-create_database.create_db(cursor,features_file=features_file,
-                          REMOVE_RECORDS=True)
-connection.commit()
-folder = "../data/debosscher"
-create_database.ingest_many_xml(folder,cursor,connection,
-                                survey="ogle",
-                                number_processors=2)
-connection.commit()
-
-
-
-
-
-
-## examine what we have collected
-sql_cmd = """SELECT source_id,survey,number_points,classification FROM sources"""
-cursor.execute(sql_cmd)
-db_info = cursor.fetchall()
-for i in db_info:
-    print i
-
-print len(db_info)
-
-##
-## remove everything not in an hipparcos, first select OGLE sources
-##
-OGLE = ("Multiple Mode Cepheid","RR Lyrae, Double Mode","Algol (Beta Persei)","Beta Lyrae","W Ursae Majoris")
-
-## make sure you are selecting the correct sources before deleting
-sql_cmd = """SELECT source_id FROM sources WHERE Classification IN""" + repr(OGLE)
-cursor.execute(sql_cmd)
-db_info = cursor.fetchall()
-for i in db_info:
-    print i
-
-## should be 517 if joey's email is correct AND my code is correct
-print(len(db_info))
-
-ogle_sources = tolist(db_info)
-
-
-
-## delete NON OGLE sources from sources and measurements tables
-sql_cmd = """DELETE FROM sources WHERE classification NOT IN""" + repr(OGLE)
-cursor.execute(sql_cmd)
-sql_cmd = """DELETE FROM measurements WHERE source_id NOT IN """ + repr(tuple(ogle_sources))
-cursor.execute(sql_cmd)
-connection.commit()
-
-
-sql_cmd = """SELECT count(*) FROM measurements"""
-cursor.execute(sql_cmd)
-db_info = cursor.fetchall()
-db_info
-sql_cmd = """SELECT count(DISTINCT source_id) FROM measurements"""
-cursor.execute(sql_cmd)
-db_info = cursor.fetchall()
-db_info
-sql_cmd = """SELECT count(*) FROM sources"""
-cursor.execute(sql_cmd)
-db_info = cursor.fetchall()
-db_info
-
-
-## remove everything with fewer than 100 flux measurements
-sql_cmd = """SELECT number_points FROM sources"""
-cursor.execute(sql_cmd)
-db_info = cursor.fetchall()
-for i in db_info:
-    print i
-
-len(db_info)
-db_info = tolist(db_info)
-
-sumList = 0
-for i in db_info:
-	sumList = i + sumList
-
-float(sumList) / len(db_info)
-
-
-sql_cmd = """SELECT source_id FROM sources WHERE number_points < 100"""
-cursor.execute(sql_cmd)
-db_info = cursor.fetchall()
-db_info
-
-db_info = tolist(db_info)
-sql_cmd = """DELETE FROM measurements WHERE source_id IN""" + repr(tuple(db_info))
-cursor.execute(sql_cmd)
-
-sql_cmd = """DELETE FROM sources WHERE number_points < 100"""
-cursor.execute(sql_cmd)
-
-## ::check what we did::
-## see how many sources in measurements and how many sources in sources
-sql_cmd = """SELECT count(DISTINCT source_id) FROM measurements"""
-cursor.execute(sql_cmd)
-db_info = cursor.fetchall()
-db_info
-sql_cmd = """SELECT count(*) FROM sources"""
-cursor.execute(sql_cmd)
-db_info = cursor.fetchall()
-db_info
-
-
-
-##############
-connection.commit()
-connection.close()
-
+######
+import load_ogle_hipparcos_cadences
 
 
 
@@ -289,7 +56,7 @@ connection.close()
 
 # make and test connection to the database
 features_file = "../db/derived_features_list.txt"
-connection = sqlite3.connect('../db/ogle_hipparocs_cadence_comparison.db')
+connection = sqlite3.connect('../db/ogle_hipparcos_cadence_comparison.db')
 cursor = connection.cursor()
 create_database.create_db(cursor,features_file=features_file,REMOVE_RECORDS=True)
 
@@ -322,7 +89,7 @@ aSurvey.generateCurve()
 tfe = np.column_stack((aSurvey.times[:,np.newaxis],
 		       aSurvey.fluxes[:,np.newaxis],
 		       aSurvey.errors[:,np.newaxis]))
-visualize.plot_curve(tfe,freq= (1 / (2*aSurvey.period_this)))
+visualize.plot_curve(tfe,period=aSurvey.period_this)
 
 
 
@@ -335,7 +102,7 @@ aSurvey.generateCurve()
 tfe = np.column_stack((aSurvey.times[:,np.newaxis],
 		       aSurvey.fluxes[:,np.newaxis],
 		       aSurvey.errors[:,np.newaxis]))
-visualize.plot_curve(tfe,freq= (1 / (2*aSurvey.period_this)))
+visualize.plot_curve(tfe,period=aSurvey.period_this)
 
 
 
@@ -389,7 +156,7 @@ tfe = np.column_stack((aSurvey.times[:,np.newaxis],
 		       aSurvey.fluxes[:,np.newaxis],
 		       aSurvey.errors[:,np.newaxis]))
 tfe[:,0].size
-visualize.plot_curve(tfe,freq= (1 / (aSurvey.period_this)))
+visualize.plot_curve(tfe,period=aSurvey.period_this)
 
 
 
@@ -537,6 +304,8 @@ derive_features.derive_features_par(source_ids,noise_dict,cursor,connection,numb
 
 
 connection.commit()
+
+
 
 #######
 ####### OUTPUT FOR ANALYSIS IN R
