eecfd120399359134782be55fcb290d9394ad41c
james <james@james-laptop.(none)>
2011-08-08 18:44:30 -0700

    much better now. can save files, smooths curves

--- src/visualize.py
+++ src/visualize.py
@@ -2,90 +2,86 @@
 ### tools for visualizing curves
 ###
 ### by James Long
-### date Dec 7, 2010
+### date August 8, 2011
+###
 
 import sqlite3
 import numpy as np
 import create_database
+import smoothers
 from matplotlib import pyplot as plt
 
+###
+### write code to save to file
+###
+
 ## shows folded and/or unfolded curves
-def plot_curve(tfe,freq=0,plot_folded=True,
-               plot_unfolded=True,classification='Unknown Class',
-               survey='Unknown'):
-    # check to make sure arguments are consistent
-    if freq == 0 and plot_folded == True:
-        print "Need a frequency to fold on. Add freq as an argument to the function call."
+def plot_curve(tfe,period=0,plot_folded=True,
+               plot_unfolded=True,smooth_folded=True,
+               classification='Unknown Class',
+               survey='Unknown Survey',
+               show_plot=True,
+               save_figure=False,
+               save_figure_name='figure.pdf'):
+
+    ## check to make sure arguments are consistent
+    if period == 0 and plot_folded == True:
+        print "Need a period to fold on. Add period as an argument to the function call."
         return
     if not plot_folded and not plot_unfolded:
-        print "No graphs requested!"
+        print "No plots requested!"
         return
 
-    # make title and phase times
-    title = classification + ' from ' + survey
-    folded = (tfe[:,0] * freq) % 1
+    ## don't want to overwrite plots
+    plt.close()
 
-    # both folded and unfolded
+    ## make title and phase times
+    title = classification + ' from ' + survey
+
+    ## both folded and unfolded
     if plot_unfolded and plot_folded:
         plt.figure(1)
         plt.subplot(211)
         plt.title(title)
-        print(max(tfe[:,0]))
         plt.plot(tfe[:,0],tfe[:,1],'r.')
         plt.xlabel("Time")
         plt.ylabel("Flux")
+        ## smooth on 
         plt.subplot(212)
-        plt.plot(folded,tfe[:,1],'r.')
+        smo = smoothers.supersmooth(tfe,2*period)
+        plt.plot(tfe[:,0],smo,tfe[:,0],tfe[:,1],'x')
+        plt.xlabel("Phased using period: " + repr(2*period))
         plt.ylabel("Flux")
-        plt.xlabel("Phase")
+        ## smooth on estimated period
+        ## plt.subplot(213)
+        ## smo = smoothers.supersmooth(tfe,1/2)
+        ## plt.plot(tfe[:,0],smo,'o',tfe[:,0],tfe[:,1],'x')
 
-    # only unfolded plot
+    ## only unfolded plot
     if plot_unfolded and not plot_folded:
         plt.plot(tfe[:,0],tfe[:,1],'r.')
         plt.title(title)
         plt.xlabel("Time")
         plt.ylabel("Flux")
 
-
-    # only folded plot
+    ## only folded plot
     if not plot_unfolded and plot_folded:
         plt.plot(folded,tfe[:,1],'r.')
         plt.title(title)
         plt.ylabel("Flux")
         plt.xlabel("Phase")
 
-    plt.show()
-    
-
-# input source_ids and access to database -> plot all the source_ids
-def plot_set_of_curves(source_ids,cursor):
-    for i in source_ids:
-        tfe = create_database.get_measurements(i,cursor)
-        sql_cmd = """SELECT classification, survey FROM sources WHERE source_id = (?)"""
-        cursor.execute(sql_cmd,[i])
-        db_info = cursor.fetchall()
-        freq = 2.58423833943 # should be a function to get the frequency 
-        plot_curve(tfe,freq=freq,classification=db_info[0][0],survey=db_info[0][1])
+    ## either save figure or show figure
+    if save_figure:
+        plt.savefig(save_figure_name)
+    if show_plot:
+        plt.show()
 
 
 
 if __name__ == "__main__":
     if 1:
-        connection = sqlite3.connect('astronomy.db')
-        cursor = connection.cursor()
-
-        # get a source_id from table
-        sql_cmd = "SELECT source_id,survey,classification FROM sources"
-        cursor.execute(sql_cmd)
-        db_info = cursor.fetchall()
-        source_ids = []
-        for i in range(len(db_info)):
-            source_ids.append(db_info[i][0])
-        plot_set_of_curves(source_ids,cursor)
-
-
-    if 0:
-        connection = sqlite3.connect('astronomy.db')
+        connection = sqlite3.connect('simulated_astronomy.db')
         cursor = connection.cursor()
 
         # get a source_id from table
