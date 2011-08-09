8d91f7d6a918aed2fdf7c398ff9984634d32dca0
james <james@james-laptop.(none)>
2011-08-08 18:45:56 -0700

    now just smoothes response and returns it rather than trying to plot. have to change spline smoother to do the same

--- src/smoothers.py
+++ src/smoothers.py
@@ -8,6 +8,10 @@ import synthetic_data
 import supsmu
 
 
+## TODO standardize to spline and supersmooth output same type of data
+## should both these functions be moved to visualize?
+
+
 ## p246 in scipy tutorial for good
 ## info on this
 
@@ -26,31 +30,37 @@ def spline(tfe,period,class_name="Unknown"):
     plt.plot(xs,ys,tfe[:,0],tfe[:,1],'x')
     plt.show()
 
-def supersmoother(tfe,period,class_name="Unknown"):
+
+## TODO: allow arguments to adjust weights, span, and alpha
+def supersmooth(tfe,period):
     tfe[:,0] = (tfe[:,0] % period) / period
     positions = tfe[:,0].argsort()
-    tfe = tfe[positions,:]
+    tfe[:,:] = tfe[positions,:]
     typevec = np.float32
     x = tfe[:,0].astype(typevec)
     y = tfe[:,1].astype(typevec)
     w = 1 / (tfe[:,2].astype(typevec))
     iper = np.array([2]).astype(np.int32)
     span = np.array([0.0]).astype(typevec)
-    alpha = np.array([-1.0]).astype(typevec)
+    alpha = np.array([.01]).astype(typevec)
     smo = np.zeros(tfe.shape[0]).astype(typevec)
     sc = np.zeros(tfe.shape[0]*7).reshape((tfe.shape[0],7)).astype(typevec)
     supsmu.supsmu(x,y,w,iper,span,alpha,smo,sc)
-    print "smo is:"
-    print smo
-    plt.figure()
-    plt.title(class_name)
-    plt.plot(tfe[:,0],smo,'o',tfe[:,0],tfe[:,1],'x')
-    plt.show()
+    return(smo)
 
 
 if __name__ == '__main__':
-	aSurvey = synthetic_data.surveySetup()
-	aSurvey.generateCurve()
-	tfe = np.c_[aSurvey.times,aSurvey.fluxes,aSurvey.errors]
-	period = aSurvey.period_this
-	smoothing_test.supersmoother(tfe,period,aSurvey.class_name)	 
+    ## practice the supersmooth function
+    if 1:
+        aSurvey = synthetic_data.surveySetup()
+        aSurvey.generateCurve()
+        tfe = np.c_[aSurvey.times,aSurvey.fluxes,aSurvey.errors]
+        period = aSurvey.period_this
+        smo = supersmooth(tfe,period)	 
+        print "smo is:"
+        print smo
+        plt.figure()
+        plt.title(aSurvey.class_name + 
+                  ' with period ' + repr(aSurvey.period_this))
+        plt.plot(tfe[:,0],smo,tfe[:,0],tfe[:,1],'x')
+        plt.show()
