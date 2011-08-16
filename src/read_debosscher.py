83b55e3deb5bf9dca01eeaa97b3d192a3032394b
james <james@james-laptop.(none)>
2011-08-15 18:17:09 -0700

    moving creation of entries in sources table out of interpreter file and into create_database file. this hasn't been tested much yet in this instance

--- src/read_debosscher.py
+++ src/read_debosscher.py
@@ -10,7 +10,7 @@
 import noisification
 import db_output
 import derive_features
-
+import create_database
 
 import create_database
 import visualize
@@ -120,19 +120,8 @@ db_info = cursor.fetchall()
 for i in db_info:
     print i
 
-# number of sources we will actually be using
+## number of sources we will actually be using
 print(len(db_info))
-
-
-## stopped using b/c problem
-## assign to test and training
-## sources with fewer than 100 flux measurements are test
-## sources with 100 or more are test
-##sql_cmd = """UPDATE sources SET survey = 'test' WHERE number_points < 100"""
-##cursor.execute(sql_cmd)
-##sql_cmd = """UPDATE sources SET survey = 'train' WHERE number_points > 100"""
-##cursor.execute(sql_cmd)
-##connection.commit()
 
 
 ## used for splitting train / test in random way
@@ -161,81 +150,50 @@ for i in db_info:
 
 
 ###
-### retreive and then noisify training
+### retreive and then noisify entries in sources
 ###
 
-# retrieve training data
+source_pragma = create_database.get_pragma(cursor,table='sources')
+n_points = [10,20,30,40,50,60,70,80,90,100]
+
+
+## retrieve training data
 sql_cmd = """SELECT source_id FROM sources WHERE survey = 'train'"""
 cursor.execute(sql_cmd)
 db_info = cursor.fetchall()
 source_ids = tolist(db_info)
-
-# create n_version noisy versions of each clean source for every value of n_points
-# put result in sources (this does not generate features)
-n_versions = 5
-n_points = np.arange(start=10,stop=51,step=5)
-column_names = ["source_id","original_source_id","classification","survey","true_period","c1","e1","c2","e2","number_points","noisification","noise_args"]
-sql_cmd = """SELECT """ + ', '.join(column_names) +  """ FROM sources WHERE source_id=(?)"""
-for j in n_points:
-	for i in source_ids:
-		cursor.execute(sql_cmd,[i])
-		db_info = cursor.fetchall()
-		curve_info = list(db_info[0])
-		curve_info[1] = curve_info[0]
-		n_points_original = curve_info[-3]
-		curve_info[-3] = int(j)
-		curve_info[-2] = "cadence_noisify"
-		sql_cmd2 = create_database.assembleSQLCommand("sources",column_names[1:])
-		# input range(n_version) of source i sampling points with small time
-		for k in range(n_versions):
-			offset  = int(math.floor((float(n_points_original - j) / (n_versions - 1)) * k))
-			curve_info[-1] = "[" + repr(j) + ",'first'," + repr(offset) + "]"
-			print curve_info
-			cursor.execute(sql_cmd2, curve_info[1:])
-		# now just randomly grab points
-		curve_info[-1] = "[" + repr(j) + ",'random']"
-		cursor.execute(sql_cmd2, curve_info[1:])
+n_versions_first = 5
+n_versions_random = 1
+for i in db_info:
+   create_database.noisify_unsmoothed_sources(cursor,i,source_pragma,
+                                              n_versions_first=n_versions_first,
+                                              n_versions_random=n_versions_random)
 
 
 
-
-
-###
-### retreive and then noisify training
-###
-
-
-# retreive test
+## retreive test data
 sql_cmd = """SELECT source_id FROM sources WHERE survey = 'test'"""        
 cursor.execute(sql_cmd)
 db_info = cursor.fetchall()
 source_ids = tolist(db_info)
-
-n_points = np.arange(start=10,stop=51,step=5)
-column_names = ["source_id","original_source_id","classification","survey","true_period","c1","e1","c2","e2","number_points","noisification","noise_args"]
-sql_cmd = """SELECT """ + ', '.join(column_names) +  """ FROM sources WHERE source_id=(?)"""
-for j in n_points:
-	for i in source_ids:
-		cursor.execute(sql_cmd,[i])
-		db_info = cursor.fetchall()
-		curve_info = list(db_info[0])
-		curve_info[1] = curve_info[0]
-		curve_info[-3] = int(j)
-		curve_info[-2] = "cadence_noisify"
-		curve_info[-1] = "[" + repr(j) + ",'first']"
-		sql_cmd2 = create_database.assembleSQLCommand("sources",column_names[1:])
-		cursor.execute(sql_cmd2, curve_info[1:])
+n_versions_first = 1
+n_versions_random = 0
+for i in db_info:
+   create_database.noisify_unsmoothed_sources(cursor,i,source_pragma,
+                                              n_versions_first=n_versions_first,
+                                              n_versions_random=n_versions_random)
 
 
 
-# make a nice view of the features table
+
+## make a nice view of the features table
 sql_cmd = """CREATE VIEW IF NOT EXISTS sources_short AS SELECT source_id,original_source_id,classification,noisification,noise_args,true_period FROM sources"""
 cursor.execute(sql_cmd)
 
 
 
 
-# display sources
+## display sources
 sql_cmd = """SELECT * FROM sources_short"""
 cursor.execute(sql_cmd)
 db_info = cursor.fetchall()
@@ -248,15 +206,11 @@ for i in db_info:
 ### DERIVE FEATURES AND TAKE A LOOK AT CURVES
 ###
 
-# retreive everything
 sql_cmd = """SELECT source_id FROM sources"""        
 cursor.execute(sql_cmd)
 db_info = cursor.fetchall()
 source_ids = tolist(db_info)
-
 noise_dict = noisification.get_noisification_dict()
-
-# derive features for sources
 derive_features.derive_features_par(source_ids,noise_dict,cursor,connection,number_processors=2,delete_existing=True)
 
 
@@ -291,7 +245,3 @@ cursor.execute(sql_cmd)
 db_info = cursor.fetchall()
 source_ids = tolist(db_info)
 db_output.tfeOutput(source_ids,cursor,'../data_processed/hipparcos/tfe00001.dat')
-
-
-
-
