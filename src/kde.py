2cdac02b82be004b7b087231a53c338879e41484
james <james@james-laptop.(none)>
2011-08-23 17:45:49 -0700

    kde function works much better now.

--- src/kde.py
+++ src/kde.py
@@ -11,24 +11,43 @@ import numpy as np
 import scipy.stats
 from matplotlib import pyplot as plt
 
-#x = np.linspace(mina,maxa)
-#h = plt.plot(x, rv.pdf(x))
-
-
+##
+## produce a few kdes
+## args: a is a dict with keys as names of variables
+## and values the realizations of r.v. to make kde from
+##
 def produceKDE(a):
-    gkde=scipy.stats.gaussian_kde(a)
-    mina = a.min()
-    maxa = a.max()
+    ## check that a satisfies requirements
+    if type(a) != type({}):
+        print "argument must be a dictionary"
+        return(0)
+    for i in a.values():
+        if i.dtype != 'float32':
+            print "each value in a must have dtype float32"
+            return(0)
+
+    ## make kdes
+    gkde = []
+    keys = a.keys()
+    for i in keys:
+        gkde.append(scipy.stats.gaussian_kde(a[i]))
+
+    ## determine borders of display window
+    mina = min(map(lambda array1:array1.min(),a.values()))
+    maxa = max(map(lambda array1:array1.max(),a.values()))
     diff = (maxa - mina)*.1
     ind = np.linspace(mina - diff,maxa + diff,101)
-    kdepdf = gkde.evaluate(ind)
+
+    ## evaluate the kde at some points
+    kdepdf = []
+    for i in gkde:
+        kdepdf.append(i.evaluate(ind))
+
+    ## plot the kdes
     plt.figure()
-    plt.plot(ind, kdepdf, label='kde', color="g")
+    for i in range(len(kdepdf)):
+        plt.plot(ind,kdepdf[i],label=keys[i])
+    ll = plt.legend(loc='upper right')
     plt.show()
 
 
-if __name__ == '__main__':
-    if 1:
-        rv = scipy.stats.pareto(3,loc=0,scale=20)
-        a = rv.rvs(1000)
-        produceKDE(a)
