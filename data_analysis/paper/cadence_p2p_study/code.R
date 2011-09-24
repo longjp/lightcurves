99dfbc04eb79100ae6f51569700c3601617cd603
james <james@james-laptop.(none)>
2011-09-23 22:54:01 -0700

    now using Rfunctions that in its own git repo, yeaaaaa

--- data_analysis/paper/cadence_p2p_study/code.R
+++ data_analysis/paper/cadence_p2p_study/code.R
@@ -3,21 +3,27 @@
 #####
 ##### by James Long
 ##### date Sept 5, 2011
+#####
 
-source('../Rfunctions.R')
+source('~/Rmodules/Rfunctions.R')
 
-## P2p scatter : median of the absolute values of the differences
-## between successive magnitudes in the raw light curve normalized
-## by the MAD around the median.
+## P2p scatter : median of the absolute
+##               values of the differences
+## between successive magnitudes
+##    in the raw light curve normalized
+## by the MAD around the median
 DubathFeature = function(fluxes){
-  num = median(abs(fluxes[2:length(fluxes)] - fluxes[1:(length(fluxes) -1)]))
+  num = (median(abs(fluxes[2:length(fluxes)]
+                    - fluxes[1:(length(fluxes) -1)])))
   den = median(abs(fluxes - median(fluxes)))
   return( num / den )
 }
 
-GenerateMeasurements = function(lc_parms,cadence=1,number_flux=200){
+GenerateMeasurements = function(lc_parms,cadence=1,
+  number_flux=200){
   mag = 1
-  period = runif(1,min=lc_parms[1]-lc_parms[2]/2,max=lc_parms[1]+lc_parms[2]/2)
+  period = runif(1,min=lc_parms[1]-lc_parms[2]/2,
+    max=lc_parms[1]+lc_parms[2]/2)
   phase = runif(1,min=0,max=1)
   times = cadence*(1:number_flux)
   folded_times = (times %% period) / period
@@ -30,16 +36,16 @@ GenerateMeasurements = function(lc_parms,cadence=1,number_flux=200){
   return(list(times,fluxes))
 }
 
-GenerateAndCompute = function(lc_parms,cadence=1,number_flux=200,number_reps=1){
+GenerateAndCompute = function(lc_parms,cadence=1,
+  number_flux=200,number_reps=1){
   vals = rep(0,number_reps)
   for(i in 1:length(vals)){
-    fluxes = GenerateMeasurements(lc_parms,cadence,number_flux)[[2]]
+    fluxes = GenerateMeasurements(lc_parms,cadence,
+      number_flux)[[2]]
     vals[i] = DubathFeature(fluxes)
   }
   return(vals)
 }
-
-
 
 
 n = 200
@@ -52,7 +58,8 @@ xlab = "p2p Median"
 number_flux = 20
 
 ### 30 minutes
-pdf(paste('dubath_feature',number_flux,'.pdf',sep=""),width=12,height=4)
+pdf(paste('dubath_feature',number_flux,'.pdf',sep=""),
+    width=12,height=4)
 par(mfcol=c(1,3),mar=c(4,4,.5,1))
 
 cadence=1/48
@@ -62,7 +69,8 @@ class2_dist = GenerateAndCompute(class2,cadence,
   number_flux=number_flux,number_reps=n)
 DrawKDES(c(class1_dist,class2_dist),
          c(rep("class1",length(class1_dist)),
-           rep("class2",length(class2_dist))),xlab=paste(xlab," - 30 min cadence",sep=""))
+           rep("class2",length(class2_dist))),
+         xlab=paste(xlab," - 30 min cadence",sep=""))
 
 
 par(mar=c(4,1,.5,1))
@@ -75,7 +83,8 @@ class2_dist = GenerateAndCompute(class2,cadence,
 
 DrawKDES(c(class1_dist,class2_dist),
          c(rep("class1",length(class1_dist)),
-           rep("class2",length(class2_dist))),xlab=paste(xlab," - 2 day cadence",sep=""))
+           rep("class2",length(class2_dist))),
+         xlab=paste(xlab," - 2 day cadence",sep=""))
 
 par(mar=c(4,1,.5,.5))
 ### 10 days
