17d316783a9fb655e8f0b0bc6edd39d4ecf87116
james <james@james-laptop.(none)>
2011-10-03 18:29:28 -0700

    updated a couple things here, a few plots for paper

--- data_analysis/OGLE/code.R
+++ data_analysis/OGLE/code.R
@@ -12,7 +12,7 @@
 rm(list=ls(all=TRUE))
 set.seed(22071985)
 
-source('../Rfunctions.R')
+source('~/Rmodules/Rfunctions.R')
 source('../denoisification_code/denoisification.R')
 source('../denoisification_code/rf_denoise.R')
 library('randomForest')
@@ -35,40 +35,19 @@ tfe = '../../data_processed/OGLE/tfe00001.dat'
 data1 = read.table(features,sep=';',header=TRUE)
 time_flux = read.table(tfe,sep=';',header=TRUE)
 
+nrow(data1)
+table(data1$sources.classification[
+ data1$sources.original_source_id == data1$features.source_id])
+sum(table(data1$sources.classification[
+ data1$sources.original_source_id == data1$features.source_id]))
+
 sum(data1$sources.survey == 'test') / 11
 sum(data1$sources.survey == 'train' & data1$features.source_id
     == data1$sources.original_source_id)
 
-### BEGIN NOT IMPORTANT
-## simple comparison of LDA and random forest on clean data
-## this code is not too important
-data1clean = subset(data1,
-  features.source_id==sources.original_source_id)
-data1cleantrain = subset(data1clean,sources.survey=="train")
-data1cleantest = subset(data1clean,sources.survey=="test")
-nrow(data1clean)
-data1_features = names(data1)[grep("features.*",names(data1))]
-to_remove = c("features.n_points","features.source_id",
-  "features.max_slope","features.min",
-  "features.linear_trend","features.max",
-  "features.weighted_average","features.median",
-  "features.freq1_harmonics_rel_phase_0")
-data1_features = data1_features[!(data1_features %in%
-  to_remove)]
-rf_formula = formula(paste("sources.classification ~ ",
-  paste(data1_features,collapse=" + ")))
-rf_formula
-rf = randomForest(rf_formula,data1cleantrain)
-lda.fit = lda(rf_formula,data1cleantrain)
-lda.predict = predict(lda.fit,newdata=data1cleantest)
-rf.predict = predict(rf,newdata=data1cleantest)
-mean(lda.predict$class != data1cleantest$sources.classification)
-mean(rf.predict != data1cleantest$sources.classification)
-### END NOT IMPORTANT
 
-
-
-## necessary for real data b/c there is no definition of true period
+## necessary for real data b/c there is
+## no definition of true period
 ## we DEFINE true_period to be period of clean sources
 true_period_df = subset(data1,
   subset=features.source_id==sources.original_source_id,
@@ -93,15 +72,83 @@ source('../denoisification_code/denoise_code.R')
 
 
 
-#### create set of kdes that joey had in his paper
-#### TODO: finalize this code and move to Rfunctions.R file
-#### QUESTIONS:
-#### 1. HOW DO WE MAKE EDGES OF KDE COME RIGHT UP TO EDGE OF PLOT
 
 
 
+####
+#### plots for paper
+####
+nrow(data1)
+table(data1$sources.survey)
+#data1test.orig = data1$sources.survey == "test" & data1$sources.original_source_id == data1$features.source_id
+data1test.orig = (data1$sources.survey == "test" &
+                  data1$features.n_points == 10)
+data1test.orig = data1[data1test.orig,]
+nrow(data1test.orig)
+
+feature.name = "features.freq1_harmonics_rel_phase_1"
+pdf('freq1_relative_phase_10flux.pdf')
+DrawKDES(data1test.orig[,feature.name],
+         data1test.orig$sources.classification,
+         location='topleft',
+         xlab="freq1_harmonics_rel_phase_1")
+dev.off()
+
+feature.name = "features.skew"
+pdf('skew10flux.pdf')
+DrawKDES(data1test.orig[,feature.name],
+         data1test.orig$sources.classification,
+         location='topright',
+         xlab="skew")
+dev.off()
 
 
+nrow(data1)
+table(data1$sources.survey)
+#data1test.orig = data1$sources.survey == "test" & data1$sources.original_source_id == data1$features.source_id
+## data1test.orig = (data1$sources.survey == "test" &
+##                   data1$sources.original_source_id ==
+##                   data1$features.source_id)
+data1test.orig = (data1$sources.survey == "test" &
+                  data1$features.n_points == 100)
+data1test.orig = data1[data1test.orig,]
+nrow(data1test.orig)
+
+feature.name = "features.freq1_harmonics_rel_phase_1"
+pdf('freq1_relative_phase_100.pdf')
+DrawKDES(data1test.orig[,feature.name],
+         data1test.orig$sources.classification,
+         location='topright',
+         xlab="freq1_harmonics_rel_phase_1")
+dev.off()
+
+feature.name = "features.skew"
+pdf('skew_100.pdf')
+DrawKDES(data1test.orig[,feature.name],
+         data1test.orig$sources.classification,
+         location='topright',
+         xlab="skew")
+dev.off()
+
+
+
+rf1 = rfClassifiers[[1]]
+
+rownames(rfClassifiers[[1]]$importance) = sub('features.',
+          '',rfClassifiers[[1]]$importance)
+
+varImpPlot(rfClassifiers[[1]])
+
+summary(rf1$call)
+rf1$call
+varImpPlot(rf1)
+plot(rf1$importance)
+
+class(rf1$importance)
+dim(rf1$importance)
+rownames(rf1$importance) = rep("a",nrow(rf1$importance))
+importance(rf1)
+varImpPlot(rf1)
 
 ####
 #### combine noise / denoise performance comparison with
