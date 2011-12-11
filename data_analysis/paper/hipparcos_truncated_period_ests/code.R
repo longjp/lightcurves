6578fcfac532dbd46065994206d4c49978e89d67
james <james@james-laptop.(none)>
2011-12-10 18:30:47 -0800

    commented code. adjusted plot a bit for joey's comments

--- data_analysis/paper/hipparcos_truncated_period_ests/code.R
+++ data_analysis/paper/hipparcos_truncated_period_ests/code.R
@@ -7,14 +7,28 @@
 ########## date: 12/10/2011 
 ########## 
 
+####
+#### NOTE: code here is awkward / bad due to poor data 
+#### collection. all the feature information is in
+#### hipparocs_dubath_sources.dat but because i didn't
+#### get class information correct there, we have to
+#### load and merge with dubath_features file. but
+#### the dubath features are never actually used
+#### 12/10/2011
+####
+
+## setup program
+rm(list=ls(all=TRUE))
+set.seed(22071985)
+
 source('~/Rmodules/Rfunctions.R')
 library('randomForest')
 library('rpart')
 options(width=50)
 
 ## get tfes and our derived features
-features = '../../data_processed/hipparcos_dubath_sources.dat'
-tfe = '../../data_processed/hipparcos_dubath_tfe.dat'
+features = '../../../data_processed/hipparcos_dubath_sources.dat'
+tfe = '../../../data_processed/hipparcos_dubath_tfe.dat'
 data1 = read.table(features,sep=';',header=TRUE)
 data1$features.freq1_harmonics_freq_0 = as.numeric(as.character(data1$features.freq1_harmonics_freq_0))
 data1$features.freq1_harmonics_freq_0[(is.na(data1$features.freq1_harmonics_freq_0))] = 10^10
@@ -29,15 +43,16 @@ xml_filenames = as.numeric(xml_filenames)
 data1$sources.xml_filename = xml_filenames
 nrow(data1)
 names(data1)
+table(data1$sources.classification)
 
-## get dubath features and classes
-filename = "dubath_features"
+## ## get dubath features and classes
+filename = "../../../data_processed/dubath_features"
 dubath.features = read.table(filename,sep="&",header=TRUE)
 table(dubath.features$class)
 dubath.names = levels(dubath.features$class)
 dubath.classes = as.character(dubath.features$class)
 dubath.names
-our.names = c("Alpha-2 Canum Venaticorum","Alpha Cygni","Beta Cephei","BE+GCAS","Multiple Mode Cepheid","W Virginus A","W Virginus B","Delta Cepheid","Delta Cepheid First Overtone","Delta Scuti","Delta Scuti Low Amplitude","Algol (Beta Persei)","Beta Lyrae","Ellipsoidal","W Ursae Majoris","Gamma Doradus","Long Period Variable","RR Lyrae AB","RR Lyrae C","RS+BY","RV Tauri","Slowly Pulsating B Star","SX Areitas")
+our.names = c("Alpha-2 CV","Alpha Cygni","Beta Cephei","BE+GCAS","Multiple Mode Cepheid","W Virginus A","W Virginus B","Delta Cepheid","Delta Cepheid First Overtone","Delta Scuti","Delta Scuti Low Amplitude","Algol (Beta Persei)","Beta Lyrae","Ellipsoidal","W Ursae Majoris","Gamma Doradus","Long Period Variable","RR Lyrae AB","RR Lyrae C","RS+BY","RV Tauri","Slowly Pulsating B Star","SX Areitas")
 name.associations = cbind(dubath.names,our.names)
 name.associations
 for(i in 1:nrow(name.associations)){
@@ -60,20 +75,24 @@ table(data1$sources.classification)
 
 table(data1$sources.classification)
 classes = c("Slowly Pulsating B Star","Long Period Variable",
-  "Alpha-2 Canum Venaticorum")
-
-
+  "Alpha-2 CV")
 feature = "features.freq1_harmonics_freq_0"
 to_use = (data1$sources.classification %in% classes &
           data1$sources.noisification == "identity")
 sum(to_use)
 pdf('frequency_hipparcos_full.pdf',width=6,height=5.7)
-par(mar=c(4.1,4,.3,.2))
+par(mar=c(4.2,4.1,.3,.2))
 DrawKDES(log(data1[to_use,feature],base=10),
          data1$sources.classification[to_use],
          xlab="log(frequency)",
-         location='topleft')
+         location='topleft',
+         density.colors=c('black','orange','blue'),
+         line.width=4,label.size=1.5,
+         ylimits=c(0,3))
 dev.off()
+
+
+
 
 ##
 sum(time_flux$source_id %in% data1$sources.original_source_id[to_use])
@@ -96,10 +115,13 @@ to_use = (data1$sources.classification %in% classes &
           data1$sources.noisification != "identity")
 sum(to_use)
 pdf('frequency_hipparcos_365.pdf',width=6,height=5.7)
-par(mar=c(4.1,4,.3,.2))
+par(mar=c(4.2,4.1,.3,.2))
 DrawKDES(log(data1[to_use,feature],base=10),
          data1$sources.classification[to_use],
          xlab="log(frequency)",
-         location='topleft')
+         location='topleft',
+         density.colors=c('black','orange','blue'),
+         line.width=4,label.size=1.5,
+         ylimits=c(0,1.5))
 dev.off()
 
