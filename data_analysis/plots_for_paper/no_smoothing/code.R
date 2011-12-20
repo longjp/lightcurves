1ae7d6334f821cf5ad2fa64d69b451f8ad23e6d0
james <james@james-laptop.(none)>
2011-12-20 01:01:04 -0800

    now producing all graphics from section 5.1 here

--- data_analysis/plots_for_paper/no_smoothing/code.R
+++ data_analysis/plots_for_paper/no_smoothing/code.R
@@ -8,15 +8,117 @@
 ########## 
 
 
-####
+
+
+
+#### FOR SYNTHETIC
 #### create two graphics, performance for simulated data and
 #### robustness for simulated data
 ####
 
+### performance for simulated
+rm(list=ls(all=TRUE))
+set.seed(22071985)
+options(width=50)
+source('~/Rmodules/Rfunctions.R')
+load('../../synthetic_no_smoothing/RData/randomForestNoisificationResults.RData')
 
-load('../../synthetic_analysis/randomForestNoisificationResults.RData')
 
 pdf('synrfNoisificationComparison.pdf')
-plotLines(errorsSD,points.levels,xlab="Number of Flux Measurements",ylab="Error",maintitle="",ymin=0)
-legend("topright",c("Naive","Random","1 x Noise","5 x Noise"),col=1:length(class.names),lwd=2,cex=1,title="Classifiers",pch=1:length(class.names))
+plotLines(errorsSD,points.levels,
+          xlab="Number of Flux Measurements",
+          ylab="Error",maintitle="",ymin=0,
+          cex.lab=1.4)
+legend("topright",c("Naive","Unordered","1 x Noise",
+                    "5 x Noise"),
+       col=1:length(class.names),lwd=4,cex=1.5,
+       title="Classifiers",pch=1:length(class.names))
 dev.off()
+
+
+### robustness for simulated
+rm(list=ls(all=TRUE))
+set.seed(22071985)
+options(width=50)
+source('~/Rmodules/Rfunctions.R')
+load('../../synthetic_no_smoothing/RData/robustnessNoisificationResults.RData')
+
+pdf('synrobustError.pdf')
+plotLines(errorsSD,points.levels,
+          xlab="Number of Flux Measurements",
+          ylab="Error",ymin=0,maintitle="",cex.lab=1.4)
+legend("topright",line.names,col=1:length(class.names),
+       lwd=4,cex=1.5,title="Classifiers",
+       pch=1:length(class.names))
+dev.off()
+
+
+
+
+
+
+
+
+#### FOR OGLE
+#### create four graphics, performance for simulated data,
+#### robustness for simulated data, and var imp plots
+####
+
+
+### performance for ogle
+rm(list=ls(all=TRUE))
+set.seed(22071985)
+options(width=50)
+source('~/Rmodules/Rfunctions.R')
+load('../../OGLE/RData/randomForestNoisificationResults.RData')
+
+
+pdf('oglerfNoisificationComparison.pdf')
+plotLines(errorsSD,points.levels,
+          xlab="Number of Flux Measurements",
+          ylab="Error",maintitle="",ymin=0,
+          cex.lab=1.4)
+legend("topright",c("Naive","Unordered","1 x Noise",
+                    "5 x Noise"),
+       col=1:length(class.names),lwd=4,cex=1.5,
+       title="Classifiers",pch=1:length(class.names))
+dev.off()
+
+
+### robustness for simulated
+rm(list=ls(all=TRUE))
+set.seed(22071985)
+options(width=50)
+source('~/Rmodules/Rfunctions.R')
+load('../../OGLE/RData/robustnessNoisificationResults.RData')
+
+pdf('oglerobustError.pdf')
+plotLines(errorsSD,points.levels,
+          xlab="Number of Flux Measurements",
+          ylab="Error",ymin=0,maintitle="",cex.lab=1.4)
+legend("topright",line.names,col=1:length(class.names),
+       lwd=4,cex=1.5,title="Classifiers",
+       pch=1:length(class.names))
+dev.off()
+
+
+#### variable importance plots for OGLE
+
+rm(list=ls(all=TRUE))
+set.seed(22071985)
+options(width=50)
+library('randomForest')
+source('~/Rmodules/Rfunctions.R')
+load('../../OGLE/RData/rfClassifiers.RData')
+for(i in 1:length(points.levels)){
+  rownames(rfClassifiers[[i]]$importance) =
+    sub('features.','',rownames(rfClassifiers[[i]]$importance))
+  pdf(paste('varImp',points.levels[i],
+                     'Pt.pdf',sep=""))
+  par(mar=c(4.5,1,1,1))
+  varImpPlot(rfClassifiers[[i]],main="",type=1,cex.lab=1.5,
+             cex=1.2)
+  dev.off()
+}
+
+
