5633dc5670ce526dd2a6b1aae000539a9e9ef7c6
james <james@james-laptop.(none)>
2012-11-15 12:27:25 -0800

    added some EDA code to look at noisified stuff

--- data_analysis/asas/code.R
+++ data_analysis/asas/code.R
@@ -26,37 +26,109 @@ time_flux = read.table(tfe,sep=';',header=TRUE)
 
 
 
-## get n_points measurement
+## add n_points column to data1 so we can
+## do diagnostics by number of flux observed
 d1 <- aggregate(rep(1,nrow(time_flux)),
                 by=list(time_flux$source_id),sum)
 names(d1) <- c("features.source_id","sources.n_points")
-data1 <- merge(data1,d1)
+data1 <- merge(data1,d1,all=TRUE)
 
 
 
 
-
-t1 <- table(Tue Nov  6 13:05:40 2012_flux$source_id)
-d1 <- data.frame(t1)
-class(d1)
-class(d1[,1])
-class(d1[,2])
-head(d1)
-as.integer(names(table(c(15,14,15))))
-names(time_flux)
-table(time_flux$source_id)
-
-head(data1)
-names(data1)
-
-
-
-
-
+## plot period versus amplitude for everything
 summary(data1$features.freq1_harmonics_freq_0)
 summary(data1$features.amplitude)
 plot(log(data1$features.freq1_harmonics_freq_0),
      log(data1$features.amplitude))
+
+## plot period versus amplitude, highlighting full light
+## curves in red
+to_use = (data1$features.source_id !=
+          data1$sources.original_source_id)
+plot(log(data1$features.freq1_harmonics_freq_0[to_use]),
+     log(data1$features.amplitude[to_use]))
+to_use = !to_use
+points(log(data1$features.freq1_harmonics_freq_0[to_use]),
+       log(data1$features.amplitude[to_use]),
+       col="red")
+
+
+
+## plot n curves, each noisy version along with
+## clean version (clean is triangle). each of n
+## curves is colored differently
+n <- 10
+orig_ids <- unique(data1$sources.original_source_id)
+orig_ids <- sample(orig_ids,n)
+data1.sub <- subset(data1,(sources.original_source_id
+                           %in% orig_ids))
+nrow(data1.sub)
+xlim <- c(min(log(data1$features.freq1_harmonics_freq_0)),
+          max(log(data1$features.freq1_harmonics_freq_0)))
+ylim <- c(min(log(data1$features.amplitude)),
+          max(log(data1$features.amplitude)))
+pch = ((1 + (data1.sub$sources.original_source_id
+             == data1.sub$features.source_id)))
+plot(log(data1.sub$features.freq1_harmonics_freq_0),
+     log(data1.sub$features.amplitude),
+     xlim=xlim,
+     ylim=ylim,
+     col=factor(data1.sub$sources.original_source_id),
+     lwd=3,
+     pch=pch)
+
+
+
+## plot the rcorbor lightcurves
+## for some reason the rcorbor do not
+## have classifications in the xml files
+## CONCLUSIONS: SOME OF THESE ARE DEFINITELY RCORBOR
+rcorbor <- c(220040,240306,241463,242999,244506,244888,247066,247575,250762,251121,251489,251638,251987,254404,256072,256221,257713,263740)
+rcorbor <- paste("100",rcorbor,".xml",sep="")
+rcorbor <- ((data1$sources.original_source_id ==
+             data1$features.source_id) &
+            (data1$sources.xml_filename %in%
+             rcorbor))
+rcorbor <- data1$features.source_id[rcorbor]
+rcorbor
+
+i <- 1
+i <- i + 1
+DrawThreeLightCurves(rcorbor[i],data1,time_flux)
+
+
+
+
+## plot period vs. amplitude for all original sources,
+## color by Rcorbor
+rcorbor <- c(220040,240306,241463,242999,244506,244888,247066,247575,250762,251121,251489,251638,251987,254404,256072,256221,257713,263740)
+rcorbor <- paste("100",rcorbor,".xml",sep="")
+data1$rcorbor <- (data1$sources.xml_filename %in%
+                  rcorbor)
+to_use <- (data1$features.source_id ==
+          data1$sources.original_source_id)
+rcorbor <- to_use & data1$rcorbor
+plot(log(data1$features.freq1_harmonics_freq_0[to_use]),
+     log(data1$features.amplitude[to_use]))
+points(log(data1$features.freq1_harmonics_freq_0[rcorbor]),
+       log(data1$features.amplitude[rcorbor]),
+       col="red",lwd=2)
+
+
+
+
+#### Questions:
+#### 1. where are discovered Rcorbor in feature space
+####    (different location than already known ones)
+#### 2. why are Rcorbor given extremely long periods
+#### 3. the density estimation problem with errors
+####   is a lot about determining which obs in low
+#### dens areas of features space are only there b/c
+#### of error, so even if rcorbor don't change location under
+#### perturbation, hopefully some uninteresting things will
+#### 4. run a density estimator and see what objects
+#### are in low density areas of feature space
 
 
 ## look at period amplitude plot for these sources
@@ -90,7 +162,7 @@ plot(log(data1$features.freq1_harmonics_freq_0),
      col=cols)
 
 ## no clustering by number of points observed, doesn't look
-## like having few values causes misclassifications
+## like having few values causes feature errors
 cols <- 1 + 1*(quantile(data1$sources.n_points,.25) > data1$sources.n_points)
 plot(log(data1$features.freq1_harmonics_freq_0),
      log(data1$features.amplitude),
