9718f5aaf4cc707237fb91ba689f28d8885c3392
james <james@james-laptop.(none)>
2012-12-05 23:16:33 -0800

    updated code for filding eclipsing RR

--- src/derive_nodb.py
+++ src/derive_nodb.py
@@ -7,32 +7,21 @@ import derive_features
 import numpy as np
 import glob
 import smoothers
-from matplotlib import pyplot as plt
-
-filename = "../data/ogle-rr-i/OGLE-BLG-RRLYR-02792.dat"
-tfe = np.fromfile(filename,sep=" ")
-tfe = tfe.reshape((tfe.size / 3, 3))
-features = derive_features.get_features(tfe)
-features.keys()
-features.values()
-filename2 = "test.dat"
+import sys
+#from matplotlib import pyplot as plt
+from multiprocessing import Pool
 
 
-derive_smoothed(filename,filename2)
-
-
-filenames
-folder = "../data/ogle-rr-i/"
-extension = ".dat"
-filepaths = glob.glob(("%s/*" + extension) % (folder))
-
-
-for i in filepaths:
-    tfe = read_tfe(i)
-    print i
-
-
-
+##
+## used so TCP won't print anywhere
+##
+class dummyStream:
+	''' dummyStream behaves like a stream but does nothing. '''
+	def __init__(self): pass
+	def write(self,data): pass
+	def read(self,data): pass
+	def flush(self): pass
+	def close(self): pass
 
 def read_tfe(filename):
     tfe = np.fromfile(filename,sep=" ")
@@ -50,7 +39,12 @@ def compute_residuals(tfe,period):
     smo = smoothers.supersmooth(tfe,period,normalize_times=False)
     tfe[:,1] = np.mean(tfe[:,1]) + tfe[:,1] - smo
 
-def derive_smoothed(file_in,file_out):
+def derive_smoothed(args):
+    print args
+    file_in = args[0]
+    file_out = args[1]
+    orig_out = sys.stdout 
+    sys.stdout = dummyStream()
     tfe = read_tfe(file_in)
     features = derive_features.get_features(tfe)
     period = 1 / float(features["freq1_harmonics_freq_0"])
@@ -66,3 +60,39 @@ def derive_smoothed(file_in,file_out):
     # plt.scatter(tfe[:,0] % period,tfe[:,1])
     # plt.savefig("lc_folded.pdf")
     write_features(features,file_out)
+    sys.stdout = orig_out
+
+## grab all filenames from folder of interest
+## grab all expected resulting filenames
+## keep non-duplicates
+## parallelize work
+
+
+if __name__ == "__main__":
+    if 0:
+        filename = "../data/ogle-rr-i/OGLE-BLG-RRLYR-02792.dat"
+        tfe = np.fromfile(filename,sep=" ")
+        tfe = tfe.reshape((tfe.size / 3, 3))
+        features = derive_features.get_features(tfe)
+        features.keys()
+        features.values()
+        filename2 = "test.dat"
+        derive_smoothed(filename,filename2)
+
+    
+    if 1:
+        folder = "../data/ogle-rr-i/"
+        extension = ".dat"
+        filepaths = glob.glob(("%s/*" + extension) % (folder))
+        filepaths = filepaths[0:10]
+        ##print filepaths
+        names = map(lambda i:i.split('/')[-1],filepaths)
+        out_folder = "../data_processed/eclipse/"
+        outfilepaths = map(lambda x: out_folder + x,names)
+        args = zip(filepaths,outfilepaths)
+        ##print args
+        p = Pool(2)
+        p.map(derive_smoothed,args)
+        #for i in filepaths:
+
+        #     derive_smoothed(i,out_folder + output)
