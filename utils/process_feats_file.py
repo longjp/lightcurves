f4bce2f9a91eaa506a7fe82127d74d7ffc164e6f
james <james@james-laptop.(none)>
2012-12-12 17:44:09 -0800

    this file is now complete, successfully compiles together feature files

--- utils/process_feats_file.py
+++ utils/process_feats_file.py
@@ -1,34 +1,39 @@
 import glob
 
+def compile_feats_file(folder,extension,fileout):
+    filepaths = glob.glob(("%s/*" + extension) % (folder))
+    feat_string = []
+    for i in filepaths:
+        f = open(i,'r')
+        feats = map(lambda x:x.split(" ")[1].replace('\n',""),f.readlines())
+        f.close()
+        feats.append(i)
+        feat_string.append(feats)
 
-folder = "../data_processed/eclipse/"
-extension = ".dat"
-filepaths = glob.glob(("%s/*" + extension) % (folder))
 
-
-feat_string = []
-
-for i in filepaths:
+    g = open(fileout,'w')
     f = open(i,'r')
-    feats = map(lambda x:x.split(" ")[1].replace('\n',""),f.readlines())
+    feats_names = map(lambda x:x.split(" ")[0].replace('\n',""),f.readlines())
     f.close()
-    feats.append(i)
-    feat_string.append(feats)
 
+    g.write(' '.join(feats_names) + " filename\n")
 
+    for i in feat_string:
+        g.write(' '.join(i) + "\n")
 
+    g.close()
 
+if __name__ == "__main__":
+    ## process original sources
+    print "processing rr_orig files"
+    folder = "../data_processed/rr_orig/"
+    extension = ".dat"
+    fileout = "../data_processed/rr_orig.dat"
+    compile_feats_file(folder,extension,fileout)
 
-fileout = "../data_processed/eclipse1.dat"
-g = open(fileout,'w')
-
-f = open(i,'r')
-feats_names = map(lambda x:x.split(" ")[0].replace('\n',""),f.readlines())
-f.close()
-
-g.write(' '.join(feats_names) + " filename\n")
-
-for i in feat_string:
-    g.write(' '.join(i) + "\n")
-
-g.close()
+    ## process residual sources
+    print "processing rr_residual files"
+    folder = "../data_processed/rr_residual/"
+    extension = ".dat"
+    fileout = "../data_processed/rr_residual.dat"
+    compile_feats_file(folder,extension,fileout)
